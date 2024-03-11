f03795bee289962914588b90799d6ecf980e6a70
Esther Guo <special0831qi@gmail.com>
2024-03-11 14:49:19 +0000

    fix no data

--- streamlit_app.py
+++ streamlit_app.py
@@ -114,37 +114,40 @@ def make_donut(input_response, input_text, input_color):
 # col = st.columns((1.5, 4.5, 2), gap='medium')
 
 # with col[0]:
-st.markdown('### Diversity')
 
 ## gender
-gender_distribution = filtered_data['Gender'].value_counts()
-female_portion = gender_distribution.get("Female", 0)
-male_portion = gender_distribution.get("Male", 0)
+if len(filtered_data):
+    st.markdown('### Diversity')
+    gender_distribution = filtered_data['Gender'].value_counts()
+    female_portion = gender_distribution.get("Female", 0)
+    male_portion = gender_distribution.get("Male", 0)
 
-donut_chart_female = make_donut(round(female_portion/len(filtered_data)*100), 'Inbound Migration', 'blue')
-donut_chart_male = make_donut(round(male_portion/len(filtered_data)*100), 'Outbound Migration', 'orange')
+    donut_chart_female = make_donut(round(female_portion/len(filtered_data)*100), 'Inbound Migration', 'blue')
+    donut_chart_male = make_donut(round(male_portion/len(filtered_data)*100), 'Outbound Migration', 'orange')
 
-gender_col = st.columns((0.2, 1, 1, 0.2))
-with gender_col[1]:
-    st.markdown('##### She / her')
-    st.altair_chart(donut_chart_female)
-with gender_col[2]:
-    st.markdown('##### He / him')
-    st.altair_chart(donut_chart_male)
+    gender_col = st.columns((0.2, 1, 1, 0.2))
+    with gender_col[1]:
+        st.markdown('##### She / her')
+        st.altair_chart(donut_chart_female)
+    with gender_col[2]:
+        st.markdown('##### He / him')
+        st.altair_chart(donut_chart_male)
 
-## generation
-diversity_distribution = filtered_data['Generational'].value_counts()
-fig = go.Figure(data=[go.Pie(labels=diversity_distribution.index, 
-                             values=diversity_distribution.values, 
-                             hole=.3, 
-                             marker_colors=px.colors.sequential.Blues,
-                             textfont_size=32,
-                             )])
-# st.write('Generation')
-# Set the figure size
-fig.update_layout(height=500, width=500) 
-st.plotly_chart(fig, use_container_width=True)
+    ## generation
+    diversity_distribution = filtered_data['Generational'].value_counts()
+    fig = go.Figure(data=[go.Pie(labels=diversity_distribution.index, 
+                                values=diversity_distribution.values, 
+                                hole=.3, 
+                                marker_colors=px.colors.sequential.Blues,
+                                textfont_size=32,
+                                )])
+    # st.write('Generation')
+    # Set the figure size
+    fig.update_layout(height=500, width=500) 
+    st.plotly_chart(fig, use_container_width=True)
 
+else:
+    st.markdown('### No Data')
 
 # with col[1]:
 if nationality_filter == default_option:
@@ -164,15 +167,16 @@ if nationality_filter == default_option:
     
 
 # with col[2]:
-st.markdown('### Pre-IAEA Work Experience')
+if len(filtered_data):
+    st.markdown('### Pre-IAEA Work Experience')
 
-# Calculate work experience background distribution
-experience_distribution = filtered_data['Pre-IAEA Work Experience'].value_counts()
+    # Calculate work experience background distribution
+    experience_distribution = filtered_data['Pre-IAEA Work Experience'].value_counts()
 
-# Plot bar chart
-fig = px.bar(experience_distribution, x=experience_distribution.index, y=experience_distribution.values)
-fig.update_layout(xaxis_title="Experience", yaxis_title="Number of Staff")
-st.plotly_chart(fig, use_container_width=True)
+    # Plot bar chart
+    fig = px.bar(experience_distribution, x=experience_distribution.index, y=experience_distribution.values)
+    fig.update_layout(xaxis_title="Experience", yaxis_title="Number of Staff")
+    st.plotly_chart(fig, use_container_width=True)
     
 with st.expander('About', expanded=True):
     st.write('''
