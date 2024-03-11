6bc4e0085ab109538f4953b23d0a9289c7fdee05
Esther Guo <special0831qi@gmail.com>
2024-03-11 14:36:16 +0000

    initial version

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,13 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import plotly.express as px
+# import streamlit_shadcn_ui as ui
+import plotly.graph_objects as go
 
 #######################
 # Page configuration
 st.set_page_config(
-    page_title="US Population Dashboard",
+    page_title="IAEA Department of Safeguards Dashboard",
     page_icon="🏂",
     layout="wide",
     initial_sidebar_state="expanded")
@@ -19,60 +21,47 @@ alt.themes.enable("dark")
 #######################
 # Load data
 df_reshaped = pd.read_csv('data/us-population-2010-2019-reshaped.csv')
+df_overview = pd.read_csv('data/staff-info.csv')
 
 
 #######################
 # Sidebar
 with st.sidebar:
-    st.title('🏂 US Population Dashboard')
+    # Default selection for filters
+    default_option = "All"
+
+    nationality_options = [default_option] + list(df_overview['Nationality'].unique())
+    nationality_filter = st.sidebar.selectbox("Select Nationality:", nationality_options, index=nationality_options.index(default_option))
     
-    year_list = list(df_reshaped.year.unique())[::-1]
+    profession_options = [default_option] + list(df_overview['IAEA Profession'].unique())
+    profession_filter = st.sidebar.selectbox("Select Profession:", profession_options, index=profession_options.index(default_option))
     
-    selected_year = st.selectbox('Select a year', year_list)
-    df_selected_year = df_reshaped[df_reshaped.year == selected_year]
-    df_selected_year_sorted = df_selected_year.sort_values(by="population", ascending=False)
+    academic_options = [default_option] + list(df_overview['Academic'].unique())
+    academic_filter = st.sidebar.selectbox("Select Academic Background:", academic_options, index=academic_options.index(default_option))
+    
+    # Filter data
+    filtered_data = df_overview.copy()
+    if nationality_filter != default_option:
+        filtered_data = filtered_data[filtered_data['Nationality'] == nationality_filter]
 
-    color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
-    selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
+    if profession_filter != default_option:
+        filtered_data = filtered_data[filtered_data['IAEA Profession'] == profession_filter]
+
+    if academic_filter != default_option:
+        filtered_data = filtered_data[filtered_data['Academic'] == academic_filter]
 
 
+st.title('🏂 IAEA Department of Safeguards Dashboard')
+
+# ui.tabs(options=['PyGWalker', 'Graphic Walker', 'GWalkR', 'RATH'], default_value='PyGWalker', key="kanaries")
+
+selected_year = 2019
+df_selected_year = df_reshaped[df_reshaped.year == selected_year]
+df_selected_year_sorted = df_selected_year.sort_values(by="population", ascending=False)
+
+selected_color_theme = 'blues'
 #######################
 # Plots
-
-# Heatmap
-def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
-    heatmap = alt.Chart(input_df).mark_rect().encode(
-            y=alt.Y(f'{input_y}:O', axis=alt.Axis(title="Year", titleFontSize=18, titlePadding=15, titleFontWeight=900, labelAngle=0)),
-            x=alt.X(f'{input_x}:O', axis=alt.Axis(title="", titleFontSize=18, titlePadding=15, titleFontWeight=900)),
-            color=alt.Color(f'max({input_color}):Q',
-                             legend=None,
-                             scale=alt.Scale(scheme=input_color_theme)),
-            stroke=alt.value('black'),
-            strokeWidth=alt.value(0.25),
-        ).properties(width=900
-        ).configure_axis(
-        labelFontSize=12,
-        titleFontSize=12
-        ) 
-    # height=300
-    return heatmap
-
-# Choropleth map
-def make_choropleth(input_df, input_id, input_column, input_color_theme):
-    choropleth = px.choropleth(input_df, locations=input_id, color=input_column, locationmode="USA-states",
-                               color_continuous_scale=input_color_theme,
-                               range_color=(0, max(df_selected_year.population)),
-                               scope="usa",
-                               labels={'population':'Population'}
-                              )
-    choropleth.update_layout(
-        template='plotly_dark',
-        plot_bgcolor='rgba(0, 0, 0, 0)',
-        paper_bgcolor='rgba(0, 0, 0, 0)',
-        margin=dict(l=0, r=0, t=0, b=0),
-        height=350
-    )
-    return choropleth
 
 
 # Donut chart
@@ -95,7 +84,7 @@ def make_donut(input_response, input_text, input_color):
       "% value": [100, 0]
   })
     
-  plot = alt.Chart(source).mark_arc(innerRadius=45, cornerRadius=25).encode(
+  plot = alt.Chart(source).mark_arc(innerRadius=90, cornerRadius=50).encode(
       theta="% value",
       color= alt.Color("Topic:N",
                       scale=alt.Scale(
@@ -104,10 +93,10 @@ def make_donut(input_response, input_text, input_color):
                           # range=['#29b5e8', '#155F7A']),  # 31333F
                           range=chart_color),
                       legend=None),
-  ).properties(width=130, height=130)
+  ).properties(width=260, height=260)
     
-  text = plot.mark_text(align='center', color="#29b5e8", font="Lato", fontSize=32, fontWeight=700, fontStyle="italic").encode(text=alt.value(f'{input_response} %'))
-  plot_bg = alt.Chart(source_bg).mark_arc(innerRadius=45, cornerRadius=20).encode(
+  text = plot.mark_text(align='center', color="#29b5e8", font="Lato", fontSize=64, fontWeight=700, fontStyle="italic").encode(text=alt.value(f'{input_response} %'))
+  plot_bg = alt.Chart(source_bg).mark_arc(innerRadius=90, cornerRadius=40).encode(
       theta="% value",
       color= alt.Color("Topic:N",
                       scale=alt.Scale(
@@ -115,113 +104,78 @@ def make_donut(input_response, input_text, input_color):
                           domain=[input_text, ''],
                           range=chart_color),  # 31333F
                       legend=None),
-  ).properties(width=130, height=130)
+  ).properties(width=260, height=260)
   return plot_bg + plot + text
 
-# Convert population to text 
-def format_number(num):
-    if num > 1000000:
-        if not num % 1000000:
-            return f'{num // 1000000} M'
-        return f'{round(num / 1000000, 1)} M'
-    return f'{num // 1000} K'
-
-# Calculation year-over-year population migrations
-def calculate_population_difference(input_df, input_year):
-  selected_year_data = input_df[input_df['year'] == input_year].reset_index()
-  previous_year_data = input_df[input_df['year'] == input_year - 1].reset_index()
-  selected_year_data['population_difference'] = selected_year_data.population.sub(previous_year_data.population, fill_value=0)
-  return pd.concat([selected_year_data.states, selected_year_data.id, selected_year_data.population, selected_year_data.population_difference], axis=1).sort_values(by="population_difference", ascending=False)
 
 
 #######################
 # Dashboard Main Panel
-col = st.columns((1.5, 4.5, 2), gap='medium')
+# col = st.columns((1.5, 4.5, 2), gap='medium')
 
-with col[0]:
-    st.markdown('#### Gains/Losses')
+# with col[0]:
+st.markdown('### Diversity')
 
-    df_population_difference_sorted = calculate_population_difference(df_reshaped, selected_year)
+## gender
+gender_distribution = filtered_data['Gender'].value_counts()
+female_portion = gender_distribution.get("Female", 0)
+male_portion = gender_distribution.get("Male", 0)
 
-    if selected_year > 2010:
-        first_state_name = df_population_difference_sorted.states.iloc[0]
-        first_state_population = format_number(df_population_difference_sorted.population.iloc[0])
-        first_state_delta = format_number(df_population_difference_sorted.population_difference.iloc[0])
-    else:
-        first_state_name = '-'
-        first_state_population = '-'
-        first_state_delta = ''
-    st.metric(label=first_state_name, value=first_state_population, delta=first_state_delta)
+donut_chart_female = make_donut(round(female_portion/len(filtered_data)*100), 'Inbound Migration', 'blue')
+donut_chart_male = make_donut(round(male_portion/len(filtered_data)*100), 'Outbound Migration', 'orange')
 
-    if selected_year > 2010:
-        last_state_name = df_population_difference_sorted.states.iloc[-1]
-        last_state_population = format_number(df_population_difference_sorted.population.iloc[-1])   
-        last_state_delta = format_number(df_population_difference_sorted.population_difference.iloc[-1])   
-    else:
-        last_state_name = '-'
-        last_state_population = '-'
-        last_state_delta = ''
-    st.metric(label=last_state_name, value=last_state_population, delta=last_state_delta)
+gender_col = st.columns((0.2, 1, 1, 0.2))
+with gender_col[1]:
+    st.markdown('##### She / her')
+    st.altair_chart(donut_chart_female)
+with gender_col[2]:
+    st.markdown('##### He / him')
+    st.altair_chart(donut_chart_male)
 
-    
-    st.markdown('#### States Migration')
+## generation
+diversity_distribution = filtered_data['Generational'].value_counts()
+fig = go.Figure(data=[go.Pie(labels=diversity_distribution.index, 
+                             values=diversity_distribution.values, 
+                             hole=.3, 
+                             marker_colors=px.colors.sequential.Blues,
+                             textfont_size=32,
+                             )])
+# st.write('Generation')
+# Set the figure size
+fig.update_layout(height=500, width=500) 
+st.plotly_chart(fig, use_container_width=True)
 
-    if selected_year > 2010:
-        # Filter states with population difference > 50000
-        # df_greater_50000 = df_population_difference_sorted[df_population_difference_sorted.population_difference_absolute > 50000]
-        df_greater_50000 = df_population_difference_sorted[df_population_difference_sorted.population_difference > 50000]
-        df_less_50000 = df_population_difference_sorted[df_population_difference_sorted.population_difference < -50000]
-        
-        # % of States with population difference > 50000
-        states_migration_greater = round((len(df_greater_50000)/df_population_difference_sorted.states.nunique())*100)
-        states_migration_less = round((len(df_less_50000)/df_population_difference_sorted.states.nunique())*100)
-        donut_chart_greater = make_donut(states_migration_greater, 'Inbound Migration', 'green')
-        donut_chart_less = make_donut(states_migration_less, 'Outbound Migration', 'red')
-    else:
-        states_migration_greater = 0
-        states_migration_less = 0
-        donut_chart_greater = make_donut(states_migration_greater, 'Inbound Migration', 'green')
-        donut_chart_less = make_donut(states_migration_less, 'Outbound Migration', 'red')
 
-    migrations_col = st.columns((0.2, 1, 0.2))
-    with migrations_col[1]:
-        st.write('Inbound')
-        st.altair_chart(donut_chart_greater)
-        st.write('Outbound')
-        st.altair_chart(donut_chart_less)
-
-with col[1]:
-    st.markdown('#### Total Population')
-    
-    choropleth = make_choropleth(df_selected_year, 'states_code', 'population', selected_color_theme)
+# with col[1]:
+if nationality_filter == default_option:
+    st.markdown('### Nationality')
+    nationality_distribution = filtered_data['Nationality'].value_counts().reset_index()
+    nationality_distribution.columns = ['Nationality', 'count']
+    choropleth = px.choropleth(nationality_distribution, 
+                        locations="Nationality",
+                        locationmode="country names",
+                        color="count",
+                        color_continuous_scale=selected_color_theme,
+                        hover_name="Nationality",
+                        projection="natural earth")
+    choropleth.update_layout(geo=dict(showcoastlines=True))
     st.plotly_chart(choropleth, use_container_width=True)
     
-    heatmap = make_heatmap(df_reshaped, 'year', 'states', 'population', selected_color_theme)
-    st.altair_chart(heatmap, use_container_width=True)
     
 
-with col[2]:
-    st.markdown('#### Top States')
+# with col[2]:
+st.markdown('### Pre-IAEA Work Experience')
 
-    st.dataframe(df_selected_year_sorted,
-                 column_order=("states", "population"),
-                 hide_index=True,
-                 width=None,
-                 column_config={
-                    "states": st.column_config.TextColumn(
-                        "States",
-                    ),
-                    "population": st.column_config.ProgressColumn(
-                        "Population",
-                        format="%f",
-                        min_value=0,
-                        max_value=max(df_selected_year_sorted.population),
-                     )}
-                 )
+# Calculate work experience background distribution
+experience_distribution = filtered_data['Pre-IAEA Work Experience'].value_counts()
+
+# Plot bar chart
+fig = px.bar(experience_distribution, x=experience_distribution.index, y=experience_distribution.values)
+fig.update_layout(xaxis_title="Experience", yaxis_title="Number of Staff")
+st.plotly_chart(fig, use_container_width=True)
     
-    with st.expander('About', expanded=True):
-        st.write('''
-            - Data: [U.S. Census Bureau](https://www.census.gov/data/datasets/time-series/demo/popest/2010s-state-total.html).
-            - :orange[**Gains/Losses**]: states with high inbound/ outbound migration for selected year
-            - :orange[**States Migration**]: percentage of states with annual inbound/ outbound migration > 50,000
-            ''')
+with st.expander('About', expanded=True):
+    st.write('''
+        - Data: [Department of Safeguards]().
+        - :orange[**Diversity**]: staff are from varied backgrounds, education, career paths, and ages
+        ''')
