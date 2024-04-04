a6b6cd5a45aeed070f7fc91f96e20e142ac9c6e7
Esther Guo <special0831qi@gmail.com>
2024-04-04 13:59:50 +0000

    card content

--- pages/🌍_Staff_Details.py
+++ pages/🌍_Staff_Details.py
@@ -34,7 +34,7 @@ css ='''
     }
 
     [data-testid="stHorizontalBlock"] {
-        align-items: center;
+        align-items: start;
         
     }
 
@@ -87,16 +87,24 @@ numOfRows =  ceil(numOfStaff/3)
 
 card_template = """
                     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-                    <div class="card text-dark bg-light mb-3" style="height: 300px">
+                    <div class="card text-dark bg-light mb-3" style="height: 250px">
                         <img src="{}" class="card-img-top mx-auto mt-3" alt="avatar" style="width: 120px"> <!-- Adjusted class for centering and margin -->
                         <div class="card-body d-flex flex-column align-items-center">
-                            <h5 class="card-title">{}</h5>
-                            <p class="font-italic">{}</p>
+                            <p style="font-size: 20px">{}</p>
+                            <p>{}</p>
                         </div>
                     </div>
             """
 gender_text = "She/her"
 avartar_url = "https://avatar.iran.liara.run/public/girl"
+
+detail_template = """
+                    <div style="display: flex; flex-direction: column;">
+                        <p>Nationality: {}</p>
+                        <p>Pre-IAEA Experience: {}</p>
+                        <p>Generational: {}</p>
+                    </div>
+            """
 
 itemIdx = 0
 for rowIdx in range(0, numOfRows):
@@ -117,74 +125,69 @@ for rowIdx in range(0, numOfRows):
                     unsafe_allow_html=True
                 )
             expander = profile.expander("More")
-            expander.markdown('''
-    The chart above shows some numbers I picked for you.
-    I rolled actual dice for these, so they're **guaranteed** to
-    be random.
-''')
+            expander.markdown(detail_template.format(
+                    staffDf["Nationality"], 
+                    staffDf["Pre-IAEA Work Experience"], 
+                    staffDf["Generational"]
+                    ), 
+                    unsafe_allow_html=True)
             itemIdx += 1
 
-# if len(filtered_data) == 0:
-#     st.write("no data")
-with st.container(border=True):
-    # cols = st.columns((1, 3, 6, 6, 3, 1))
-    # fields = ["", "Staff ID", 'Name', 'IAEA Profession', "Action", ""]
-    cols = st.columns((1,2,3,2))
-    fields = ["Staff ID", 'Name', 'IAEA Profession', "Action"]
-    for col, field_name in zip(cols, fields):
-        # header
-        if field_name:
-            col.markdown(f'**{field_name}**')
+# with st.container(border=True):
+#     cols = st.columns((1,2,3,2))
+#     fields = ["Staff ID", 'Name', 'IAEA Profession', "Action"]
+#     for col, field_name in zip(cols, fields):
+#         # header
+#         if field_name:
+#             col.markdown(f'**{field_name}**')
 
-    for idx, staffId in enumerate(filtered_data["Staff ID"]):
-        # col0, col1, col2, col3, col4, col5 = st.columns((1, 3, 6, 6, 3, 1))
-        col1, col2, col3, col4 = st.columns((1, 2, 3, 2))
+#     for idx, staffId in enumerate(filtered_data["Staff ID"]):
+#         col1, col2, col3, col4 = st.columns((1, 2, 3, 2))
 
-        # col0.empty()
-        col1.write(str(staffId))
-        col2.write(filtered_data["Name"][idx])
-        col3.write(filtered_data["IAEA Profession"][idx])
-        btn_placeholder = col4.empty()
-        # col5.empty()
+#         # col0.empty()
+#         col1.write(str(staffId))
+#         col2.write(filtered_data["Name"][idx])
+#         col3.write(filtered_data["IAEA Profession"][idx])
+#         btn_placeholder = col4.empty()
+#         # col5.empty()
         
-        clicked = btn_placeholder.button(label="See Profile", key=staffId)
-        if clicked:
-            card_template = """
-                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-                        <div class="card text-black bg-light mb-3" >
-                            <div class="row no-gutters">
-                                <div class="col-md-4">
-                                    <img src="{}" class="w-50 m-5" alt="avatar">
-                                </div>
-                                <div class="col-md-8">
-                                    <div class="card-body">
-                                        <h5 class="card-title">{}({})</h5>
-                                        <p class="font-italic">{}</p>
-                                        <p class="card-text"><span class=" font-weight-bold">Nationality:</span> {}</p>
-                                        <p class="card-text"><strong>Pre IAEA Background:</strong> {}</p>
-                                        <p class="card-text">{}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-            """
-            gender_text = "She/her"
-            avartar_url = "https://avatar.iran.liara.run/public/girl"
-            if filtered_data["Gender"][idx] == "Male":
-                gender_text = "He/him"
-                avartar_url = "https://avatar.iran.liara.run/public/boy"
+#         clicked = btn_placeholder.button(label="See Profile", key=staffId)
+#         if clicked:
+#             card_template = """
+#                     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+#                         <div class="card text-black bg-light mb-3" >
+#                             <div class="row no-gutters">
+#                                 <div class="col-md-4">
+#                                     <img src="{}" class="w-50 m-5" alt="avatar">
+#                                 </div>
+#                                 <div class="col-md-8">
+#                                     <div class="card-body">
+#                                         <h5 class="card-title">{}({})</h5>
+#                                         <p class="font-italic">{}</p>
+#                                         <p class="card-text"><span class=" font-weight-bold">Nationality:</span> {}</p>
+#                                         <p class="card-text"><strong>Pre IAEA Background:</strong> {}</p>
+#                                         <p class="card-text">{}</p>
+#                                     </div>
+#                                 </div>
+#                             </div>
+#                         </div>
+#             """
+#             gender_text = "She/her"
+#             avartar_url = "https://avatar.iran.liara.run/public/girl"
+#             if filtered_data["Gender"][idx] == "Male":
+#                 gender_text = "He/him"
+#                 avartar_url = "https://avatar.iran.liara.run/public/boy"
             
-            st.markdown(
-                card_template.format(
-                    avartar_url, 
-                    filtered_data["Name"][idx], 
-                    gender_text,
-                    filtered_data["IAEA Profession"][idx], 
-                    filtered_data["Nationality"][idx], 
-                    filtered_data["Pre-IAEA Work Experience"][idx], 
+#             st.markdown(
+#                 card_template.format(
+#                     avartar_url, 
+#                     filtered_data["Name"][idx], 
+#                     gender_text,
+#                     filtered_data["IAEA Profession"][idx], 
+#                     filtered_data["Nationality"][idx], 
+#                     filtered_data["Pre-IAEA Work Experience"][idx], 
 
-                    "Some brief introduction text to introduce the staff's portfolio. " 
-                    ), 
-                    unsafe_allow_html=True
-                )
-# st.session_state
+#                     "Some brief introduction text to introduce the staff's portfolio. " 
+#                     ), 
+#                     unsafe_allow_html=True
+#                 )
